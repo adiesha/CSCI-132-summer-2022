887f63aca023277460ad301330e553288b2b31ce
Adiesha <adiesha@gmail.com>
2022-06-24 13:20:10 -0600

    Added the presentation for introduction

---
+++ CodeAndResourses/src/main/java/org/csci132/summer/App.java
@@ -0,0 +1,13 @@
+package org.csci132.summer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
