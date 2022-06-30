d7920ce71a758cefacb7ff65e82ca878d5f40ed0
Adiesha <adiesha@gmail.com>
2022-06-30 17:13:05 -0600

    Added commandline example

---
+++ CommandlineExample/HelloWorld.java
@@ -0,0 +1,8 @@
+package csci1;
+
+import org.apache.commons.lang3.StringUtils;
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ CommandlineExample/StringRotate.java
@@ -0,0 +1,10 @@
+package csci1.Rotate;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class StringRotate {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println(StringUtils.rotate("abcdefg", 2));
+    }
+}
