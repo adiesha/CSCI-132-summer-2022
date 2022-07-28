989561963ce5ccf593bb648e77c3564a88357ac8
Adiesha <adiesha@gmail.com>
2022-07-28 13:16:05 -0600

    Added Lab 7 skeleton

---
+++ Labs/Lab7/Lab7/src/main/java/org/csci132/DuplicatedWord.java
@@ -0,0 +1,18 @@
+package org.csci132;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * @author : Adiesha
+ * @created : 7/28/2022, Thursday
+ **/
+public class DuplicatedWord {
+    public static void wordFrequency(String input) {
+        // create a dictionary that can have keep Strings as keys and Integers as values
+        // iterate the input string word by word
+        // Input the word to the dictionary as a key if it does not already contain
+
+    }
+}
