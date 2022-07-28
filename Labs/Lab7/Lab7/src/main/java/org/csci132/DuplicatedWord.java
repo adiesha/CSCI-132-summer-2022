8601406ccafcd591da40aa6ac4473c0d928508bb
Adiesha <adiesha@gmail.com>
2022-07-28 13:19:03 -0600

    Added Lab 7 comments

--- Labs/Lab7/Lab7/src/main/java/org/csci132/DuplicatedWord.java
+++ Labs/Lab7/Lab7/src/main/java/org/csci132/DuplicatedWord.java
@@ -1,9 +1,5 @@
 package org.csci132;
 
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-
 /**
  * @author : Adiesha
  * @created : 7/28/2022, Thursday
@@ -12,7 +8,11 @@ public class DuplicatedWord {
     public static void wordFrequency(String input) {
         // create a dictionary that can have keep Strings as keys and Integers as values
         // iterate the input string word by word
-        // Input the word to the dictionary as a key if it does not already contain
+        // convert the word into lowercase word
+        // remove unnecessary punctuation symbols
+        // Input the word to the dictionary as a key if it does not contain the word, and the value 1
+        // If the word already is in the dictionary update the value associated with that key by incrementing it by 1
+        // Once you finish processing these words print each word and its frequency of occurrence in the sentence
 
     }
 }
